ed8865ff6ef324e219ddafcf74af2078969392af
David Dolan <exinmusic@gmail.com>
2025-05-29 07:36:19 -0700

    included state setter for testing

--- set_state_tester.py
+++ set_state_tester.py
@@ -5,6 +5,7 @@ client = base.Client(('localhost', 11211))
 data = {
     "player_connect": True,
     "score": 140,
-    "player_name": "ZEBBLER"
+    "player_name": "ZEBBLER",
+    "id": "0003030-003"
 }
 client.set("badge_data", json.dumps(data))
